ac290c19bcbfc871c85166f081e4e9a7733423e2
Esauceda <saucedaedu15@gmail.com>
2021-03-09 13:16:30 -0600

    Primer commit de prueba

---
+++ src/hn/edu/ujcv/pii/p1/Main.java
@@ -0,0 +1,18 @@
+package hn.edu.ujcv.pii.p1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner teclado=new Scanner(System.in);
+        int numero1, numero2, suma;
+        System.out.println("Ingrese el Numero 1: ");
+        numero1 = teclado.nextInt();
+        System.out.println("Ingrese el Numero 2: ");
+        numero2 = teclado.nextInt();
+        suma = numero1 + numero2;
+        System.out.println("La suma es: " + suma);
+    }
+}
